9d5d6f91c0bd44a6132aeb4fd2270108095b9fe4
hutingjin <179645132@qq.com>
2020-08-03 23:50:13 +0800

    first commit

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-require("./lib.js")
-for(let i of [1,2,3]){
-    console.log(i)
-}
-console.log("main")
+// require("./lib.js")
+// for(let i of [1,2,3]){
+//     console.log(i)
+// }
+// console.log("main")
 
+import { ToyReact } from './ToyReact.js'
 let a = <Mycomponent name='a'></Mycomponent>
